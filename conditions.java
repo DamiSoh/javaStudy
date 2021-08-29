a80d95254a192b90b741de19f99b8d69c2379f55
DamiSoh <83524438+DamiSoh@users.noreply.github.com>
2021-08-30 01:38:17 +0900

    new

--- conditions.java
+++ conditions.java
@@ -7,5 +7,18 @@ public class conditions {
         if (x < y){
             System.out.println("x는 y보다 작습니다.");
         }
+
+        x = 60;
+
+        if (x == y)
+            System.out.println("x는 y와 같습니다.");
+            // if문에 {} 줄 수도 있고, 안 줄 수도 있다. 안 줄 때는 한 라인만 작성 가능하다.
+
+        if (x>y){
+            System.out.println("-- x가 y보다 큽니다.");
+        }else {
+            System.out.println("-- x는 y와 같거나 작습니다.");
+        }
+
     }
 }
